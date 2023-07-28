2d289f518d43a51b07cf0f15033ced154bad755e
SiroProg <070777sss@gmail.com>
2023-07-28 09:18:57 +0500

    add Exception , factory constructor, getter and setters

--- main.dart
+++ main.dart
@@ -1,1 +1,29 @@
-class Word {}
+class Word {
+  String _word;
+
+  String get word => _word;
+  set word(String text) {
+    try {
+      if (RegExp(r'[a-z]').hasMatch(text)) {
+        _word = text;
+        return;
+      }
+      throw Exception('Invalid word!');
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Word(this._word);
+
+  factory Word.replace({required String newWord}) {
+    try {
+      if (!RegExp(r'[A-Z0-9]').hasMatch(newWord)) {
+        return Word(newWord);
+      }
+      throw Exception('InvalidWord');
+    } catch (e) {
+      throw e;
+    }
+  }
+}
